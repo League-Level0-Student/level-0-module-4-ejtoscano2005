5311585066c53787decc4bd214f99cb44a39d63d
Student_2 <Student_2@165.24.36.193>
2018-06-04 15:48:06 -0700

    Happy Pet, Pi Aloud, Simple Calculator.

--- src/HappyPet.java
+++ src/HappyPet.java
@@ -1,29 +1,66 @@
 import javax.swing.JOptionPane;
 
 public class HappyPet {
-	
-	// 2. Add the following variable to the next line: static int happinessLevel = 0;
+
+	// 2. Add the following variable to the next line: static int happinessLevel =
+	// 0;
 	// this will be used to store the happiness of your pet
-	
+	static int happinessLevel = 0;
+
 	public static void main(String[] args) {
-		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
-
+		// 1. Ask the user what kind of pet they want to buy, and store their answer in
+		// a variable
+		String pet = JOptionPane.showInputDialog("What kind of pet do you want to buy?");
 		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
-		
-			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
-			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
-			//    Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Button1", "Button2", "Button3" }, null);
+		for (int i = 0; i < 100; i++) {
 
+			// 3. Use showOptionDialog to ask the user what they want to do to make their
+			// pet happy
+			// String pet = JOptionPane.showInputDialog("?");
+			// (eg: cuddle, food, water, take a walk, groom, clean up poop).
+			// Make sure to customize the title and question too.
+			int task = JOptionPane.showOptionDialog(null, "What do you want to do to make your pet happy", "Happiness",
+					0, JOptionPane.INFORMATION_MESSAGE, null, new String[] { "take a walk", "food", "water" }, null);
+			System.out.println(task);
 			// 5. Use user input to call the appropriate method created in step 4.
+			if (task == 0) {
+				takeAWalk();
+			}
+
+			if (task == 1) {
+				food();
+			}
+
+			if (task == 2) {
+				water();
+			}
 
 			// 6. If you determine the happiness level is large enough, tell the
-			//    user that he loves his pet and use break; to exit for loop.
+			// user that he loves his pet and use break; to exit for loop.
+			if (happinessLevel >= 100) {
+				JOptionPane.showMessageDialog(null, "You love your pet!");
+				break;
+			}
+		}
+	}
+	// 4. Create methods to handle each of your user selections.
+	// Each method should create a pop-up with the pet's response (eg. cat might
+	// purr when pet),
+	// and INCREMENT the pet's happiness Level.
 
+	static void takeAWalk() {
+		JOptionPane.showMessageDialog(null, "Bwark");
+		happinessLevel += 15;
 	}
 
-	// 4. Create methods to handle each of your user selections.
-	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
-	//    and INCREMENT the pet's happiness Level.
+	static void food() {
+		JOptionPane.showMessageDialog(null, "Aarf");
+		happinessLevel += 10;
+	}
+
+	static void water() {
+		JOptionPane.showMessageDialog(null, "Woof");
+		happinessLevel += 5;
+	}
+
 }
--- src/SimpleCalculator.java
+++ src/SimpleCalculator.java
@@ -4,22 +4,46 @@ import javax.swing.JOptionPane;
 
 public class SimpleCalculator {
 	public static void main(String[] args) {
-
+for(int i=0; ;i++ ) {
 		// 1. Get 2 numbers from the user and convert them to integer.
-
+		String num = JOptionPane.showInputDialog("Give me a number");
+		String num2 = JOptionPane.showInputDialog("Give me another number?");
+		int x=Integer.parseInt(num);
+		int y=Integer.parseInt(num2);
 		// 2. Customize pop-up to support add/subtract/multiply/divide operations.
-		int operation = JOptionPane.showOptionDialog(null, "The question", "Pop-up Title", 0,
-				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Button1", "Button2", "Button3", "Button3" },
+		int operation = JOptionPane.showOptionDialog(null, "Do you want to add, subtract, multiply, or divide", "Pop-up Title", 0,
+				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "add", "subtract", "mulyiply", "divide" },
 				null);
 
 		// 5. Call the methods created in steps 3 and 4 to perform the appropriate operation.
-
+if(operation==0){add(x,y);}
+if(operation==1){sub(x,y);}
+if(operation==2){mult(x,y);}
+if(operation==3){div(x,y);}
 		// 6. Wrap steps 1, 2 and 5 in a for loop to repeat calculator operations
 	}
-
+	}
 	// 3. Create method for addition operation.
 	//    Method should create pop-up that shows equation and solution, eg. 45 + 25 = 70.
 	//    HINT: use 'static void add(int num1, num2) { ... }
-
+	static void add(int n1, int n2) {
+		JOptionPane.showMessageDialog(null, n1 + "+" + n2 + "=" + (n1+n2));
+		
+	}
 	// 4. Create similar methods for subtraction, multiplication and division.
+	static void sub(int n1, int n2) {
+		JOptionPane.showMessageDialog(null, n1 + "-" + n2 + "=" + (n1-n2));
+		
+	}
+	static void mult(int n1, int n2) {
+		JOptionPane.showMessageDialog(null, n1 + "*" + n2 + "=" + (n1*n2));
+		
+	}
+	static void div(int n1, int n2) {
+		JOptionPane.showMessageDialog(null, n1 + "/" + n2 + "=" + (n1/n2));
+		
+	}
+
+
+
 }
